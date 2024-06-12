5f4db8df9bcf53422bf3282158c699ae55a5e6c7
Owen-Ross <rossow@sheridancollege.ca>
2024-06-12 16:56:55 -0400

    Integrated React and Redux

--- get-out-of-here-goblins/src/index.js
+++ get-out-of-here-goblins/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Game from './containers/Game';
+import reducer from './reducers';
+//import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <h1>Creating a game using react and redux!</h1>
-  </React.StrictMode>
+  <Provider store={store}>
+    <Game />
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//registerServiceWorker();
